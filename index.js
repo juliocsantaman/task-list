7e06ecaab3fe3aabd831364953e40cd045147ae3
juliocsantaman <juliocsantaman@gmail.com>
2020-05-28 01:13:53 -0500

    First commit. Add task.

---
+++ index.js
@@ -0,0 +1,26 @@
+//Variables.
+const btnAdd = document.getElementById('btn-add');
+const taskList = document.getElementById('task-list');
+
+//Add a task.
+function addTask() {
+  let li = document.createElement('li');
+  let task = document.getElementById('add');
+
+  if(task.value == '') {
+    alert('Error: No puedes tener una tarea en blanco');
+    return;
+  }
+
+  li.innerHTML = `${task.value} <span class="deleting-icon">X</span> `
+  taskList.appendChild(li);
+
+  task.value = '';
+}
+
+btnAdd.addEventListener('click', addTask);
+
+
+
+
+
