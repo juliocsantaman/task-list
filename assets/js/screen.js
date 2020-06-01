321c009dc7861bb1c768ad7c78d2e1c4b43fafa7
juliocsantaman <juliocsantaman@gmail.com>
2020-06-01 13:22:34 -0500

    Doing responsive screen.

--- assets/js/screen.js
+++ assets/js/screen.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
   let height = window.screen.height;
   let main = document.querySelector('main');
-  main.style.height = height + "px";
+  main.style.minHeight = height + "px";
   console.log(height);
 });
